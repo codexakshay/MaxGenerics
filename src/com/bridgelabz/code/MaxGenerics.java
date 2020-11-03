1f2f2b4bf94c52890ebfbd8dbfa97efa7cf2b66c
codexakshay <codexakshay@gmail.com>
2020-11-03 08:02:49 +0530

    MaxGenerics : Maximum Float

--- src/com/bridgelabz/code/MaxGenerics.java
+++ src/com/bridgelabz/code/MaxGenerics.java
@@ -5,6 +5,9 @@ public class MaxGenerics {
 	public int compare(Integer objectA, Integer objectB) {
 		return objectA.compareTo(objectB);
 	}
+	public int compare(Float objectA, Float objectB) {
+		return objectA.compareTo(objectB);
+	}
 
 	public static int maxInt(Integer a, Integer b, Integer c) {
 		int max = a;
@@ -15,8 +18,19 @@ public class MaxGenerics {
 			max = c;
 		return max;
 	}
+	
+	public static float maxFloat(Float a, Float b, Float c) {
+		float max = a;
+
+		if (b.compareTo(a) > 0)
+			max = b;
+		if (c.compareTo(b) > 0)
+			max = c;
+		return max;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Max Integer : " + maxInt(123, 1234, 12345));
+		System.out.println("Max Float : " + maxFloat(123.0f, 1234.0f, 12345.0f));
 	}
 }
--- src/com/bridgelabz/code/TestMaxGenerics.java
+++ src/com/bridgelabz/code/TestMaxGenerics.java
@@ -15,20 +15,38 @@ public class TestMaxGenerics {
 	}
 
 	@Test
-	public void TestMaxOnFirstPosition_Happy() {
+	public void TestMaxOnFirstPositionInt_Happy() {
 		int result = MaxGenerics.maxInt(123, 12, 1);
 		Assert.assertEquals(123, result);
 	}
 
 	@Test
-	public void TestMaxOnSecondPosition_Happy() {
+	public void TestMaxOnSecondPositionInt_Happy() {
 		int result = MaxGenerics.maxInt(11, 22, 3);
 		Assert.assertEquals(22, result);
 	}
 
 	@Test
-	public void TestMaxOnThirdPosition_Happy() {
+	public void TestMaxOnThirdPositionInt_Happy() {
 		int result = MaxGenerics.maxInt(1, 12, 123);
 		Assert.assertEquals(123, result);
+	}
+	
+	@Test
+	public void TestMaxOnFirstPositionFloat_Happy() {
+		float result = MaxGenerics.maxFloat(123.0f, 12.0f, 1.0f);
+		Assert.assertEquals(123.0f, result);
+	}
+
+	@Test
+	public void TestMaxOnSecondPositionFloat_Happy() {
+		float result = MaxGenerics.maxFloat(11.0f, 22.0f, 3.0f);
+		Assert.assertEquals(22.0f, result);
+	}
+
+	@Test
+	public void TestMaxOnThirdPositionFloat_Happy() {
+		float result = MaxGenerics.maxFloat(1.0f, 12.0f, 123.0f);
+		Assert.assertEquals(123.0f, result);
 	}
 }
